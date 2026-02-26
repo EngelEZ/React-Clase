e691bd5904723afc3a8e6cd1033a115fb9692eb4
EngelEZ <sneiderpro.email@gmail.com>
2026-02-25 20:55:19 -0500

    feat: se agrega componente navbar y componente link de react-router-dom

--- frontend/vitalplus-react/src/app/App.jsx
+++ frontend/vitalplus-react/src/app/App.jsx
@@ -1,18 +1,7 @@
-import UserForm from "../features/users/components/UserForm";
-import DeleteCounter from "../shared/components/DeleteCounter";
-import DeleteUseEfecct from "../shared/components/DeleteUseEffect";
+import { RouterProvider } from "react-router-dom";
+import router from "@/app/router/index";
 
 export default function App() {
-  return (
-    <div className="grid grid-cols-1 gap-6 justify-items-center">
-      <h1 className="bg-brand text-brand-soft text-2xl font-bold">
-        Vital-Plus
-      </h1>
-
-      <UserForm></UserForm>
-      {/* <LoginForm></LoginForm> */}
-      <DeleteCounter></DeleteCounter>
-      <DeleteUseEfecct></DeleteUseEfecct>
-    </div>
-  );
-}
+  return (<RouterProvider router={router} />
+    )
+  }
---
+++ frontend/vitalplus-react/src/shared/layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "@/shared/layout/Navbar";
+
+export default function MainLayout() {
+  return (
+    <div className="min-h-screen bg-surface">
+      {/* Navbar */}
+      <Navbar />
+
+      <main className="mx-auto max-w7 px-4 py-6">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
