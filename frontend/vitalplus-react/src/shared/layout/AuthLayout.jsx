40901eb5878bb1920c8aae29bc85def7e25ffee7
EngelEZ <sneiderpro.email@gmail.com>
2026-03-04 21:39:26 -0500

    Proteger archivos con index.js

--- frontend/vitalplus-react/src/shared/layout/AuthLayout.jsx
+++ frontend/vitalplus-react/src/shared/layout/AuthLayout.jsx
@@ -1,25 +1,24 @@
 import { Outlet } from "react-router-dom";
 import authBg from "@/assets/images/prueba.jpg";
-import UserForm from "@/features/users/components/UserForm";
+import { UserForm } from "@/features/users";
 
 export default function AuthLayout() {
+  return (
+    <div
+      className="relative min-h-screen w-full flex items-center justify-center text-black"
+      style={{
+        backgroundImage: `url(${authBg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      {/* <div className="absolute inset-0 -z-10  bg-white/60" /> */}
 
-  return(
-  <div
-    className="relative min-h-screen w-full flex items-center justify-center text-black"
-    style={{
-      backgroundImage: `url(${authBg})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-    }}
-  >
-    {/* <div className="absolute inset-0 -z-10  bg-white/60" /> */}
-
-    {/* Contenido externo que se inyecta  */}
-    <main className="mx-auto">
-      <Outlet />
-      <UserForm/>
-    </main>
-  </div>
-  )
+      {/* Contenido externo que se inyecta  */}
+      <main className="mx-auto">
+        <Outlet />
+        <UserForm />
+      </main>
+    </div>
+  );
 }
